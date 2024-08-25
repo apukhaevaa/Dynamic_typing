ccb766adc6383ec2b713804f3308c4b2213ca8ec
apukhaevaa <apukhaevaa@gmail.com>
2024-08-25 19:06:54 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,11 @@
+# Пухаева Алина Александровна
+# Dynamic typing
+# 25.08.2024
+name = 'Alina'
+print(name, type(name))
+age = 31
+print(age, type(age))
+age = age + 0.5
+print(age, type(age))
+is_student = True
+print(is_student, type(is_student))
